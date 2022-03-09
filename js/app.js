1eae6ec590d8d7be28f3b7e179f6c560b071176a
Mhmd11abdelmoniem <101283177+Mhmd11abdelmoniem@users.noreply.github.com>
2022-03-09 20:34:45 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//nav bar style when scroll
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
+
+//collapse//expand answers
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className==='uil uil-plus'){
+            icon.className = 'uil uil-minus';
+        }else{
+            icon.className = 'uil uil-plus';
+        }
+    })
+})
